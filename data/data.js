194a8b18a8e827aa747d4de657128444a10c321e
Linaris1993 <linakhatmullina@gmail.com>
2021-02-25 12:56:10 -0500

    data commit

--- data/data.js
+++ data/data.js
@@ -2,17 +2,20 @@
 const standardUser = {
     login: "standard_user",
     password: "secret_sauce"
+
 }
 const text = {
     LowestPriceItem: "$7.99",
     finalTotal: "Total: $28.06",
     productLabelHomePage: "Products",
     finalPageLabel: "Finish"
-}
 
+}
 const userInfo = {
     firstName: "testName",
     lastName: "testLastName",
     zip: "00000"
+
 }
+
 export { standardUser, text, userInfo };
